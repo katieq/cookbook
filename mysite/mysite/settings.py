2dcea5e50dc95fe2f4ec6337415c27ae2896bcbf
katieq <quinn.katiej@gmail.com>
2017-02-02 21:33:38 -0800

    css formatting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'cooklog.apps.CooklogConfig',
+    'django.contrib.humanize',
 )
 
 MIDDLEWARE_CLASSES = (
