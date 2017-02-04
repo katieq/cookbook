337d213e82e385f7ab47c4774b743c9bcc6212f0
katieq <quinn.katiej@gmail.com>
2017-02-04 09:44:23 -0800

    css changes, mostly to home

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -104,6 +104,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.8/howto/static-files/
 
 STATIC_URL = '/static/'
+PROJECT_DIR = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_DIR, 'static')
 
 # Media root and url, kq added Jan-11-2017 (aiming for location as mysite/cooklog/media/..)
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
